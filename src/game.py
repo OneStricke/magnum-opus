08880b4d0dea688871927592333c2566353138f6
OneChell <pavellitvak600@gmail.com>
2026-05-23 14:25:19 +0200

    added object class and gravity

---
+++ src/game.py
@@ -0,0 +1,16 @@
+from physics import ObjectInSpace
+
+earth = ObjectInSpace(300, 400, 1, 0, 0)
+G = 10
+
+def apply_gravity(movee: ObjectInSpace, mover: ObjectInSpace, dt):
+    force_x, force_y = mover.get_gravity_force(movee, G)
+    movee.x_vel += force_x * movee.mass * dt
+    movee.y_vel += force_y * movee.mass * dt
+    movee.x_cog += movee.x_vel * dt
+    movee.y_cog += movee.y_vel * dt
+
+# # Check impact
+# if earth.check_collision(asteroid.pos, asteroid.radius):
+#     # award points, trigger explosion, etc.
+#     pass
